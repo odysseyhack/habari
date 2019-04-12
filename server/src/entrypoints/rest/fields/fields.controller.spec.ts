e847db0cf640a6d83822530efad02e9913712577
Tom Veugelers <tom.veugelers-isc@nl.ibm.com>
2019-04-13 00:07:17 +0200

    Add fields endpoint tests

--- server/src/entrypoints/rest/fields/fields.controller.spec.ts
+++ server/src/entrypoints/rest/fields/fields.controller.spec.ts
@@ -15,4 +15,39 @@ describe('Fields Controller', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  describe('create', () => {
+    it('should return all fields', async () => {
+      const result = { 'data': 'TODO: implement fields POST endpoint' };
+      expect(await controller.create()).toEqual(result);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return all fields', async () => {
+      const result = { 'data': 'TODO: implement fields GET endpoint' };
+      expect(await controller.findAll()).toEqual(result);
+    });
+  });
+
+  describe('findById', () => {
+    it('should return all fields', async () => {
+      const result = { 'data': 'TODO: implement fields GET:id endpoint', 'id': '42' };
+      expect(await controller.findById('42')).toEqual(result);
+    });
+  });
+
+  describe('update', () => {
+    it('should return all fields', async () => {
+      const result = { 'data': 'TODO: implement fields PUT:id endpoint', 'id': '42' };
+      expect(await controller.update('42')).toEqual(result);
+    });
+  });
+
+  describe('remove', () => {
+    it('should return all fields', async () => {
+      const result = { 'data': 'TODO: implement fields DELETE:id endpoint', 'id': '42' };
+      expect(await controller.remove('42')).toEqual(result);
+    });
+  });
 });
