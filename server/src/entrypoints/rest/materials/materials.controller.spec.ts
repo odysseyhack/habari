1ef3bd1c933a8ea75d79fb68aef4fc5d33a4bcb3
Tom Veugelers <tom.veugelers-isc@nl.ibm.com>
2019-04-13 08:49:56 +0200

    Add materials endpoint tests

--- server/src/entrypoints/rest/materials/materials.controller.spec.ts
+++ server/src/entrypoints/rest/materials/materials.controller.spec.ts
@@ -15,4 +15,39 @@ describe('Materials Controller', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  describe('create', () => {
+    it('should return all materials', async () => {
+      const result = { 'data': 'TODO: implement materials POST endpoint' };
+      expect(await controller.create()).toEqual(result);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return all materials', async () => {
+      const result = { 'data': 'TODO: implement materials GET endpoint' };
+      expect(await controller.findAll()).toEqual(result);
+    });
+  });
+
+  describe('findById', () => {
+    it('should return all materials', async () => {
+      const result = { 'data': 'TODO: implement materials GET:id endpoint', 'id': '42' };
+      expect(await controller.findById('42')).toEqual(result);
+    });
+  });
+
+  describe('update', () => {
+    it('should return all materials', async () => {
+      const result = { 'data': 'TODO: implement materials PUT:id endpoint', 'id': '42' };
+      expect(await controller.update('42')).toEqual(result);
+    });
+  });
+
+  describe('remove', () => {
+    it('should return all materials', async () => {
+      const result = { 'data': 'TODO: implement materials DELETE:id endpoint', 'id': '42' };
+      expect(await controller.remove('42')).toEqual(result);
+    });
+  });
 });
