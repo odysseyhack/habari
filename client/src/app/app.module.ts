266c75d8c1f0c8f66bea8f105be8a8b7896622e3
Joao Gonçalves <goncalves@evident.nl>
2019-04-13 12:46:42 +0200

    redofrontend

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,27 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from './app.component';
-import { KonamiModule } from 'ngx-konami';
-import { LoginComponent } from './login/login.component';
 import { routing } from './app.routing';
-import { RegisterComponent } from './register/register.component';
-import { FarmerSideComponent } from './farmer-side/farmer-side.component';
-import { DonatorSideComponent } from './donator-side/donator-side.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    FarmerSideComponent,
-    DonatorSideComponent,
-    DashboardComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    KonamiModule,
     routing
   ],
   providers: [],
--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -1,10 +1,6 @@
 import { Routes, RouterModule } from '@angular/router';
-import { AuthGuard } from './_guards';
 
 const appRoutes: Routes = [
-    { path: '', component: HomeComponent, canActivate: [AuthGuard] },
-    { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegisterComponent },
 
     // otherwise redirect to home
     { path: '**', redirectTo: '' }
