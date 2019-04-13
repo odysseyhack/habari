6f0d57cf2c20dfecb878e7ba142fe5752d884e95
Joao Gonçalves <goncalves@evident.nl>
2019-04-13 12:58:15 +0200

    remove bad client

--- client/src/app/app.routing.ts
+++
@@ -1,9 +0,0 @@
-import { Routes, RouterModule } from '@angular/router';
-
-const appRoutes: Routes = [
-
-    // otherwise redirect to home
-    { path: '**', redirectTo: '' }
-];
-
-export const routing = RouterModule.forRoot(appRoutes);
